8ac9c58b632be2c7ea557f83386e74999e43802d
shogo_inamoto <shogo.ca2011+dev@gmail.com>
2017-08-07 21:33:25 +0900

    Create single.php

---
+++ single.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+<div class="wrap">
+  <main>
+    <div class="post">
+      <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+          <div class="header">
+            <h1 class="title"><?php the_title(); ?></h1>
+            <div class="date"><?php the_time('Y/m/d') ?></div>
+          </div>
+
+          <div class="body">
+            <?php the_content(); ?>
+          </div>
+        </article>
+      <?php endwhile; ?>
+      <?php endif; ?>
+    </div>
+  </main>
+
+</div>
+<?php get_footer(); ?>
