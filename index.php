4ba41462be072ca0f8e11847711de8c8372d67ce
shogo_inamoto <shogo.ca2011+dev@gmail.com>
2017-08-07 19:13:28 +0900

    Responsive

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <div class="wrap">
   <main>
     <div class="top-post-list">
-      <?php if (have_posts()) : while ( have_posts() ) : the_post(); ?>
+      <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
           <a href="<?php the_permalink(); ?>">
             <div class="entry-left">
